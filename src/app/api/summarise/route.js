a400dd7b68b507add47a4d8398456b35ab574753
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-11-15 16:17:21 +0100

    single pipeline file

--- src/app/api/summarise/route.js
+++ src/app/api/summarise/route.js
@@ -2,20 +2,23 @@
 // For more information, see https://nextjs.org/docs/app/building-your-application/routing/router-handlers
 
 import { NextResponse } from 'next/server'
-import PipelineSingleton from './pipeline.js';
+import PipelineSingleton from '../../lib/pipeline.js';
 
 export async function GET(request) {
     const text = request.nextUrl.searchParams.get('text');
+
+    const task = 'summarization';
+    const model = 'Xenova/distilbart-cnn-6-6';
+
     if (!text) {
         return NextResponse.json({
             error: 'Missing text parameter',
         }, { status: 400 });
     }
 
-    const summarise = await PipelineSingleton.getInstance();
+    const summarise = await PipelineSingleton.getInstance(task, model);
 
     const result = await summarise(text, {
-        max_new_tokens: 300,
         min_new_tokens: 100
     });
 
--- src/app/lib/pipeline.js
+++ src/app/lib/pipeline.js
@@ -3,14 +3,13 @@ import { pipeline } from "@xenova/transformers";
 // Use the Singleton pattern to enable lazy construction of the pipeline.
 // NOTE: We wrap the class in a function to prevent code duplication (see below).
 const P = () => class PipelineSingleton {
-    static task = 'text-classification';
-    static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
     static instance = null;
 
-    static async getInstance(progress_callback = null) {
+    static async getInstance(task, model, progress_callback = null) {
         if (this.instance === null) {
-            this.instance = pipeline(this.task, this.model, { progress_callback });
+            this.instance = pipeline(task, model, { progress_callback });
         }
+        
         return this.instance;
     }
 }
